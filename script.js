dbec18c84bb569f6850394bc9e661f0d320bb8e2
Guilherme <p.galmeidafernandes@gmail.com>
2023-06-26 12:29:43 -0300

    Att Funcionando
    Finalizado JS

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 function calculo(){
-
     const btnEl = document.getElementById("btn");
 
     const diaEl = document.getElementById("dia");
     const mesEl = document.getElementById("mes");
     const anoEl = document.getElementById("ano");
-
+    
     const dataAtual = new Date();
 
     const diaAtual = dataAtual.getDate();
     const mesAtual = dataAtual.getMonth();
     const anoAtual = dataAtual.getFullYear();
 
-    const calcDia = diaAtual - diaEl;
-    const calcMes = mesAtual - mesEl;
-    const calcAno = anoAtual - anoEl;
+    const calcDia = diaAtual - diaEl.value;
+    const calcMes = mesAtual - mesEl.value;
+    const calcAno = anoAtual - anoEl.value;
 
+    
     document.getElementById("dias").innerHTML = calcDia;
     document.getElementById("meses").innerHTML = calcMes;
     document.getElementById("anos").innerHTML = calcAno;
-
 }
