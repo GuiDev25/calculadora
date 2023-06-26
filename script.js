bb110334b37dee8dba23e157987ede7c0c980c70
Guilherme <p.galmeidafernandes@gmail.com>
2023-06-26 13:33:11 -0300

    Finalizado
    Adc de métodos de controle e cálculos de datas

--- script.js
+++ script.js
@@ -11,12 +11,16 @@ function calculo(){
     const mesAtual = dataAtual.getMonth();
     const anoAtual = dataAtual.getFullYear();
 
-    const calcDia = diaAtual - diaEl.value;
-    const calcMes = mesAtual - mesEl.value;
-    const calcAno = anoAtual - anoEl.value;
+    const calcDia = Math.abs(diaAtual - diaEl.value);
+    const calcMes = Math.abs(mesAtual - mesEl.value);
+    const calcAno = Math.abs(anoAtual - anoEl.value);
 
-    
+    if(anoEl.value>anoAtual || mesEl.value>12 || diaEl.value>31){
+        alert("Data de nascimento inválida! Por favor digite novamente");
+        location.reload();
+    }else{
     document.getElementById("dias").innerHTML = calcDia;
     document.getElementById("meses").innerHTML = calcMes;
     document.getElementById("anos").innerHTML = calcAno;
+    }
 }
